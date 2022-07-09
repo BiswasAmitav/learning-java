bbc61ee0039662b086c14d48a08a4a3fea7e3e92
BiswasAmitav <biswasamit2648@gmail.com>
2022-07-09 10:30:15 +0530

    in this code i came to know how to invoke method by this key word

--- thisKeyword/src/detailsAboutThisKeyword/MyThisKeyword.java
+++ thisKeyword/src/detailsAboutThisKeyword/MyThisKeyword.java
@@ -2,9 +2,15 @@ package detailsAboutThisKeyword;
 import java.util.*;
 import java.lang.String;
 public class MyThisKeyword {
-
+//INSTANCE VARIABLE IN CURRENT CLASS INTERMS OF MyThisKeyord  CONSTUCTIOR
     String firstName;
     String lastName;
+
+
+    void msg(){
+        System.out.print("Your full name is : ");
+    }
+
 
     MyThisKeyword(String firstName, String lastName){
         //HERE COMPILER WILL PRINT NULL VALUE ....TO OVEROCME THIS SITUATION WE NEED THIS KEYWORD
@@ -14,12 +20,15 @@ public class MyThisKeyword {
 
         this.firstName=firstName;//CALLED INSTANCE VARIABLE MEANS THE CURRENT VARIABLE...
         this.lastName=lastName;//CALLED INSTANCE VARIABLE MEANS THE CURRENT VARIABLE...
+        //this.msg();//INVOKE MSG METHOD BY THIS KEWYWORD....
+
+        msg();//IF WE DONTWRITE THIS THEN ONLY IT WILL WORK , BCZ COMPILER DEFAULTLY WRITE A THIS THEN ITS WORK...
     }
     //FOR DISPLAYING THE NUMBERS...
 
     void display(){
 //        System.out.println("First Name : " + firstName + "Last name : " + lastName);
-        System.out.println("Your full name is : " + firstName + " "+ lastName);
+        System.out.println(/*"Your full name is : " + */  firstName + " "+ lastName);
     }
 
     public static void main(String[] args){
