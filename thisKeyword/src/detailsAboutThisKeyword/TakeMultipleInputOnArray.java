a67bbd8583419ff8d9ae153c37000c585cf133e8
BiswasAmitav <biswasamit2648@gmail.com>
2022-07-10 23:21:23 +0530

    by this code i learnt how to take arry inut for multiple element

--- thisKeyword/src/detailsAboutThisKeyword/TakeMultipleInputOnArray.java
+++ thisKeyword/src/detailsAboutThisKeyword/TakeMultipleInputOnArray.java
@@ -1,14 +1,33 @@
 package detailsAboutThisKeyword;
-
+import java.util.Scanner;
 public class TakeMultipleInputOnArray {
     public static void main(String[] args){
 
-//        System.out.println("Number of element : ");
-//        Scanner sc = new Scanner(System.in);
-//        int numOfElement=sc.nextInt();
+        System.out.println("Number of element : ");
+        Scanner sc = new Scanner(System.in);
+        int numOfElement=sc.nextInt();
 
-        //int array[2]=[2,3];
-        //System.out.println(array);
+        int[] arrayNumber = new int[numOfElement];
+
+        for(int i=0;i<numOfElement;i++){
+            int serial=1+i;
+            System.out.println("Enter your "+ serial +"th" +" element of array");
+            arrayNumber[i] = sc.nextInt();
+        }
+        System.out.println(
+                "Your entire array is : "
+        );
+        for(int i=0;i<arrayNumber.length;i++){
+            System.out.print(arrayNumber[i]+" ");
+        }
+
+
+    /*    arrayNumber[0] = 15;
+        arrayNumber[1] = 20;
+        System.out.println(arrayNumber[0]);
+        System.out.println(arrayNumber[1]);
+
+     */
 
     }
 }
