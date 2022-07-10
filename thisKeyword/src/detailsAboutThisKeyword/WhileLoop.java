5b0d14742ab1634f8e8f533b56eeabfc6d43b4ec
BiswasAmitav <biswasamit2648@gmail.com>
2022-07-10 21:00:40 +0530

    java while loop

---
+++ thisKeyword/src/detailsAboutThisKeyword/WhileLoop.java
@@ -0,0 +1,17 @@
+package detailsAboutThisKeyword;
+
+public class WhileLoop {
+    public static void main(String[] args){
+        int number = 21;
+
+
+        //boolean myLoop=20>15;
+        while(number>12){
+            System.out.println("I am from while Loop..");
+            number = number+2;
+            System.out.println(number);
+            break;
+        }
+
+    }
+}
