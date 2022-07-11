e8be86dffe604129b48575f605a988dc6756bc12
BiswasAmitav <biswasamit2648@gmail.com>
2022-07-11 11:05:09 +0530

    in this code i learnt how to return value form method how to use that returned value in others places

---
+++ JavaLearningSqn/src/BeginnerToAdvanceLessons/ClassMethodsObject.java
@@ -0,0 +1,32 @@
+package BeginnerToAdvanceLessons;
+
+class Amit{
+
+    int a;
+
+    void display(){
+        System.out.println("I am amit");
+    }
+
+    int calci(){
+        //this.a=a;
+        //System.out.println("your given a value is : " + a);
+        int result=a*a;
+        return result;
+    }
+}
+public class ClassMethodsObject {
+    public static void main(String[] args) {
+//        System.out.println("hello");
+//        System.out.println();
+//
+        Amit amit = new Amit();
+        amit.a=2;
+        int square = amit.calci();
+
+
+        amit.calci();
+        System.out.println(square);
+
+    }
+}
