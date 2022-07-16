2d37b21f6dc2172d38e1f9c583f0811098eea66e
BiswasAmitav <biswasamit2648@gmail.com>
2022-07-16 06:24:19 +0530

    String buffer details i learnt

---
+++ JavaLearningSqn/src/BeginnerToAdvanceLessons/StringBuilder.java
@@ -0,0 +1,31 @@
+//package BeginnerToAdvanceLessons;
+////import java.StingBuilder;
+//
+//
+//public class StringBuilder {
+//    public static void main(String[] args) {
+//        System.out.println("here i am going to learn the string builder.....");
+//
+//
+//        //Actually here three individual string is created and the cancated to first one .....
+//        //it takes some extra memory in your storage...thats why STING BUILDER IS CAME OUT....
+//
+//        /* String example1="Hello..!";
+//        example1 +="My name is ";
+//        example1 +="Amitav Biswas..";
+//
+//        System.out.println(example1);*/
+//
+//
+//        //NOW PLAY WITH STRING BUILDER....
+//      /*  StringBuilder example2 =new StringBuilder();
+//        example2.append("hello ! ");
+//        example2.append("My name is : ");
+//        example2.append("MIKE");
+//
+//
+//        System.out.println(example2);  */
+//
+//    }
+//
+//
