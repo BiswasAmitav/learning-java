7afebd2e5cf05304f3e085fbbeffaa589f99ffd0
BiswasAmitav <biswasamit2648@gmail.com>
2022-07-24 11:08:57 +0530

    by this code i learnt runtime error, which compiler can't be handle we need to save our p
    cls
    ioe
    okao

---
+++ JavaLearningSqn/src/handling/exception/runtime_exception/RuntimeException.java
@@ -0,0 +1,25 @@
+package handling.exception.runtime_exception;
+
+public class RuntimeException {
+    public static void main(String[] args) {
+        //ITS ALSO CALLED UNCHECKED EXCEPTION....
+//    int value = 11;
+//    value = value/0;
+
+        //String empty = null;
+
+       // System.out.println(empty.length());//IT GIVES U ERROR AND SAYS is null
+       // at handling.exception.runtime_exception.RuntimeException.main ....ITS CALLED NO POINTER EXCEPTION ERROR
+
+        String[] numbers = {"one", "two", "three", "four"};
+       // System.out.println(numbers[4]);//IT GIVES YOU TO ARRAY OUT OF BOUNDS we need to fixed it .....
+
+
+
+
+    }
+
+
+
+
+}
